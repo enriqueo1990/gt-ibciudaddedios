9d47577e5d83df1e232ce0037fc932e854e65b94
Iglesia Biblica de la Gracia <ibg@MacBookPro.fibertel.com.ar>
2026-04-14 15:52:30 -0300

    feat: migrar routing manual a react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Creencias from './Creencias.tsx';
 import Liderazgo from './Liderazgo.tsx';
@@ -12,10 +13,22 @@ import Articulos from './Articulos.tsx';
 import ArticuloDetalle from './ArticuloDetalle.tsx';
 import './index.css';
 
-const path = window.location.pathname;
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {path === '/creencias' ? <Creencias /> : path === '/liderazgo' ? <Liderazgo /> : path === '/eventos' ? <Eventos /> : path === '/visitanos' ? <Visitanos /> : path === '/sermones' ? <Sermones /> : path === '/sermon' ? <SermonDetalle /> : path === '/serie' ? <SerieDetalle /> : path === '/articulos' ? <Articulos /> : path === '/articulo' ? <ArticuloDetalle /> : <App />}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/creencias" element={<Creencias />} />
+        <Route path="/liderazgo" element={<Liderazgo />} />
+        <Route path="/eventos" element={<Eventos />} />
+        <Route path="/visitanos" element={<Visitanos />} />
+        <Route path="/sermones" element={<Sermones />} />
+        <Route path="/sermones/:slug" element={<SermonDetalle />} />
+        <Route path="/series/:slug" element={<SerieDetalle />} />
+        <Route path="/articulos" element={<Articulos />} />
+        <Route path="/articulos/:slug" element={<ArticuloDetalle />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
